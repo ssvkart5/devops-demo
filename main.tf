fd1e0419a5bedb7c4806851a25a4f6f97b60df09
ssvkart5 <ssvkart5@gmail.com>
2022-09-22 13:41:14 +0000

    code developed through variables

--- main.tf
+++ main.tf
@@ -12,65 +12,90 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# create VPC
-resource "aws_vpc" "devops-vpc" {
-    cidr_block = "192.168.0.0/16"
+# create vpc
+resource "aws_vpc" "mylab-vpc" {
+    cidr_block = var.cidr_block[0]
 
     tags = {
-        name = "devops-vpc"
+        Name = "mylab-vpc"
     }
 }
 
-# create subnet
-resource "aws_subnet" "devops-public-subnet-1" {
-    vpc_id = aws_vpc.devops-vpc.id 
-    cidr_block = "192.168.10.0/24"
+# create subnets
+resource "aws_subnet" "mylab-subnet-1" {
+    vpc_id = aws_vpc.mylab-vpc.id 
+    cidr_block = var.cidr_block[1]
 
     tags = {
-        name = "devops-public-subnet-1"
+        Name = "mylab-subnet-1"
     }
 }
 
-resource "aws_subnet" "devops-public-subnet-2" {
-    vpc_id = aws_vpc.devops-vpc.id 
-    cidr_block = "192.168.20.0/24"
+resource "aws_subnet" "mylab-subnet-2" {
+    vpc_id = aws_vpc.mylab-vpc.id 
+    cidr_block = var.cidr_block[2]
 
     tags = {
-        name = "devops-public-subnet-2"
+        Name = "mylab-subnet-2"
     }
 }
 
-# create internet gateway
-resource "aws_internet_gateway" "devops-igw" {
-    vpc_id = aws_vpc.devops-vpc.id 
+# crete internet gateway
+resource "aws_internet_gateway" "mylab-igw" {
+    vpc_id = aws_vpc.mylab-vpc.id
 
     tags = {
-        name = "devops-igw" 
-    }
+        Name = "mylab-igw"
+    } 
 }
 
 # create security group
-resource "aws_security_group" "devops-sg" {
-    name = "devops-sg"
-    description = "to allow inbound and outbound traffic for my devops app"
-    vpc_id = aws_vpc.devops-vpc.id 
+resource "aws_security_group" "mylab-sg" {
+  name = "mylab-sg"
+  description = "to allow inbound and outbound traffic"
+  vpc_id = aws_vpc.mylab-vpc.id 
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  dynamic ingress {
+      iterator = port
+      for_each = var.ports
+      content {
+          from_port = port.value
+          to_port = port.value
+          protocol = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+     }
+   
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-
-    }
-
-    tags = {
-        name = "allow traffic"
-    }
+  tags = {
+    Name = "mylab-sg"
+  }
 }
+
+# create route tables
+ resource "aws_route_table" "mylab-rtb" {
+  vpc_id = aws_vpc.mylab-vpc.id 
+
+  tags = {
+      Name = "mylab-rtb"
+    }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mylab-igw.id 
+
+    }
+ }
+
+ resource "aws_route_table_association" "mylab-assn" {
+   subnet_id = aws_subnet.mylab-subnet-1.id 
+  # subnet_id = aws_subnet.mylab-subnet-2.id 
+   route_table_id = aws_route_table.mylab-rtb.id 
+ }
+      
